599182f647a2b57ad07e74d9fde0f003cbc106ef
thongphamvn <duythong2707@gmail.com>
2023-07-04 14:18:18 +0700

    update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,30 @@
 import { Analytics } from '@vercel/analytics/react'
+import { Metadata } from 'next'
 import { Nunito_Sans } from 'next/font/google'
 import './globals.css'
 import { Providers } from './providers'
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'thongpham.dev',
+  icons: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      url: '/favicon/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      url: '/favicon/favicon-16x16.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      url: '/favicon/apple-touch-icon.png',
+    },
+  ],
 }
 
 const font = Nunito_Sans({ subsets: ['latin'], weight: '400' })
